8583fcc28aada5fff470291f8842498b46483f29
AliciaPavao <aliciapavao369@gmail.com>
2025-02-18 10:52:06 -0300

    Organizando códigos

--- App.py
+++ App.py
@@ -1,50 +1,26 @@
 import random
 
+from .data import listas_configuracao as listas
 from flask import Flask, render_template, request, redirect
 
 # Padrão de criação do app
 app = Flask(__name__)
 
-# Lista de cores para alterar o fundo do site
-lista_cores = [
-    "#9ff5d1",
-    "#96ffea",
-    "#6cd9c5",
-    "#60d1b3",
-    "#b5ffd9"]
-
-# Lista de adjetivos para o site
-lista_adjetivos = [
-    "Inteligentes.",
-    "Bonitas.",
-    "Velozes.",
-    "Animais marinhos.",
-    "Verdes.",
-    "Amigaveis.",
-    "Importantes para o meio ambiente."]
-
-# Lista de imagens
-lista_imagens = [
-    "01.jpg",
-    "02.jpg",
-    "03.jpg",
-    "04.jpg",
-    "05.jpg"]
 
 # Aqui irá todas as minhas rotas
 
 # Pag sobre
 @app.route("/sobre")
 def pag_sobre():
-    cor_de_fundo = random.choice(lista_cores)
+    cor_de_fundo = random.choice(listas.lista_cores)
     return render_template("PagInicial.html", cor_de_fundo_html = cor_de_fundo)
 
 # Pag infos
 @app.route("/", methods=["GET"])
 def pag_infos():
-    cor_de_fundo = random.choice(lista_cores)
-    adjetivos_aleatorios = random.choice(lista_adjetivos)
-    imagens_aleatorias = random.choice(lista_imagens)
+    cor_de_fundo = random.choice(listas.lista_cores)
+    adjetivos_aleatorios = random.choice(listas.lista_adjetivos)
+    imagens_aleatorias = random.choice(listas.lista_imagens)
     return render_template("PagAleatorio.html", cor_de_fundo_html = cor_de_fundo, 
                            adjetivos_aleatorios_html = adjetivos_aleatorios, imagens_aleatorias_html = imagens_aleatorias)
 
@@ -52,12 +28,12 @@ def pag_infos():
 # Cor
 @app.route("/cores", methods=["GET"])
 def pag_cores():
-     return render_template("PagCor.html", cores = lista_cores)
+     return render_template("PagCor.html", cores = listas.lista_cores)
 
 @app.route("/post/cadastrarcor", methods=["POST"])
 def post_cadastrarcor():
     cor_vinda_do_html = request.form.get("cor")
-    lista_cores.append(cor_vinda_do_html)
+    listas.lista_cores.append(cor_vinda_do_html)
     return redirect("/cores")
 
 @app.route("/cores/delete/<indice_cor>", methods=["GET"])
@@ -65,19 +41,19 @@ def delete_cores(indice_cor):
     # Lembrar de converter o int para inteiro - pois ele vem como string
     indice_cor = int(indice_cor)
     # Excluir a cor da lista através do indice
-    lista_cores.pop(indice_cor)
+    listas.lista_cores.pop(indice_cor)
     return redirect("/cores")
 
 
 # Escrever
 @app.route("/escreva", methods=["GET"])
 def pag_escreva():
-     return render_template("PagEscreva.html", frases = lista_adjetivos)
+     return render_template("PagEscreva.html", frases = listas.lista_adjetivos)
 
 @app.route("/post/cadastrarfrase", methods=["POST"])
 def post_cadastrarfrase():
     frase_vinda_do_html = request.form.get("frase")
-    lista_adjetivos.append(frase_vinda_do_html)
+    listas.lista_adjetivos.append(frase_vinda_do_html)
     return redirect("/escreva")
 
 # Post - Envia uma informção
---
+++ data/listas_configuracao.py
@@ -0,0 +1,25 @@
+# Lista de cores para alterar o fundo do site
+lista_cores = [
+    "#9ff5d1",
+    "#96ffea",
+    "#6cd9c5",
+    "#60d1b3",
+    "#b5ffd9"]
+
+# Lista de adjetivos para o site
+lista_adjetivos = [
+    "Inteligentes.",
+    "Bonitas.",
+    "Velozes.",
+    "Animais marinhos.",
+    "Verdes.",
+    "Amigaveis.",
+    "Importantes para o meio ambiente."]
+
+# Lista de imagens
+lista_imagens = [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg"]
